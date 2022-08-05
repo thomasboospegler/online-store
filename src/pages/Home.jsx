22ddf84cd2ba9d71874a5db926ccf8a9dc682970
Rubens Deola <rdeola@gmail.com>
2022-08-05 18:44:31 -0300

    req 04 group 8

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  state = {
+    categoriesList: [],
+  }
+
+  componentDidMount = async () => {
+    const list = await getCategories();
+    // const { categoriesList } = this.state;
+    console.log('list: ', list);
+    this.setState({ categoriesList: list });
+  }
+
   render() {
+    const { categoriesList } = this.state;
+    // console.log('categoriesList: ', categoriesList);
     return (
-      <span
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </span>
+      <div>
+        <div className="home-categories-container">
+          <div className="home-categories">
+            {/* <ul> */}
+            {categoriesList.map(({ id, name }) => (
+              <button
+                type="button"
+                data-testid="category"
+                value={ id }
+                key={ id }
+              >
+                {name}
+              </button>))}
+            {/* </ul> */}
+          </div>
+        </div>
+        <span
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </span>
+      </div>
     );
   }
 }
